e97a2daa7986419960c5775f6c5efead86a4b3e5
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-03-24 21:30:44 +0100

    The Increment and Decrement Operator

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,8 +1,4 @@
-let levelUp = 10;
-levelUp += 5
-let powerLevel = 9001;
-powerLevel -= 100
-let multiplyMe = 32;
-multiplyMe *= 11
-let quarterMe = 1152;
-quarterMe /= 4
+let gainedDollar = 3;
+gainedDollar++;
+let lostDollar = 50;
+lostDollar--;
