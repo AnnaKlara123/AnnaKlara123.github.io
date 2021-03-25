03aab8951220a067561e829b22fc9f8e453a8066
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-03-25 17:12:48 +0100

    Route.js

--- nz/main.js
+++ nz/main.js
@@ -4,19 +4,23 @@ let stop = {
     name: "Dunedine",
     lat: -45.874167,
     lng: 170.503611,
-    user: "webmapping",
+    user: "AnnaKlara123",
     wikipedia: "https://en.wikipedia.org/wiki/Dunedin"
 };
 
 const map = L.map("map", {
     center: [ stop.lat, stop.lng],
     zoom: 13,
-    layer: [
+    layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 
-let mrk = L.marker([-45.874167, 170.503611]).addTo(map);
-mrk.bindPopup("Dunedine").openPopup();
+
+
+let mrk = L.marker([ stop.lat, stop.lng ]).addTo(map);
+mrk.bindPopup(`<h4>Stop ${stop.nr}: ${stop.name}<h4>
+<p><a href="${stop.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
+`).openPopup();
 
 console.log(document.querySelector("#map"));
