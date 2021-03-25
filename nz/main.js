8011609cd05a451887952d1eabcb68edf9f58e7c
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-03-25 16:39:23 +0100

    Karte fehlt

--- nz/main.js
+++ nz/main.js
@@ -1,13 +1,22 @@
 
-const map = L.map ("map", {
-    center: [-45.874167, 170.503611],
+let stop = {
+    nr: 7,
+    name: "Dunedine",
+    lat: -45.874167,
+    lng: 170.503611,
+    user: "webmapping",
+    wikipedia: "https://en.wikipedia.org/wiki/Dunedin"
+};
+
+const map = L.map("map", {
+    center: [ stop.lat, stop.lng],
     zoom: 13,
-    layer : [
+    layer: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
-    });
+});
 
-let mrk = L.marker([-45.874167, 170.503611]) .addTo(map);    
+let mrk = L.marker([-45.874167, 170.503611]).addTo(map);
 mrk.bindPopup("Dunedine").openPopup();
 
 console.log(document.querySelector("#map"));
