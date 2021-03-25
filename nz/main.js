488ba7bbc33659d61fe8488766883d6cdc93c0bb
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-03-25 16:21:12 +0100

    Marker hizufügen

--- nz/main.js
+++ nz/main.js
@@ -3,8 +3,8 @@ const map = L.map ("map", {
     center: [-45.874167, 170.503611],
     uzoom: 13,
     layer : [
-        L.tileLayer("https://{s}.title.openstreetmap.org//{z}/{x}/{y}.png")
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
     });
-//
+// WMTS-Sercvices
 console.log(document.querySelector("#map"));
