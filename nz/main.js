b83fff608e6ffb9721c16dd77b755db0bbc1b80e
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-03-25 16:22:54 +0100

    Popup hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -1,10 +1,13 @@
 
 const map = L.map ("map", {
     center: [-45.874167, 170.503611],
-    uzoom: 13,
+    zoom: 13,
     layer : [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
     });
-// WMTS-Sercvices
+
+let mrk = L.marker([-45.874167, 170.503611]) .addTo(map);    
+mrk.bindPopup("Dunedine").openPopup();
+
 console.log(document.querySelector("#map"));
