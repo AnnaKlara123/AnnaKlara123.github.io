4553d171bcf820ce592dde97e46bad384d515fa1
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-03-25 20:12:04 +0100

    sortierung korrigiert

--- nz/main.js
+++ nz/main.js
@@ -20,9 +20,13 @@ console.log('Navigation HTML Element: ', nav);
 //console.log(ROUTE);
 
 ROUTE.sort((stop1, stop2) => {
-    return stop1.nr > stop2.nr
-});
-
+    if (stop1.nr > stop2.nr) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    
 for (let entry of ROUTE) {
     //  console.log(entry);
     nav.innerHTML += `
