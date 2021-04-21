d3bea009be8c44c54d3fb8c0f0c8d3248d4939ba
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-04-21 13:21:35 +0200

    Farben Temperatur hinzugefügt colors.js

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -28,10 +28,93 @@ const COLORS = {
         max: 999,
         col: "#ff3232"
     }],
-    temperature: [
 
-    ],
-    snowheight: [
-
+    temperature: [{
+        min: -999,
+        max: -25,
+        col: "#9f80ff"
+    }, {
+        min: -25,
+        max: -20,
+        col: "#784cff"
+    }, {
+        min: -20,
+        max: -15,
+        col: "#0f5abe"
+    }, {
+        min: -15,
+        max: -10,
+        col: "#1380ff"
+    }, {
+        min: -10,
+        max: -5,
+        col: "#19cdff"
+    }, {
+        min: -5,
+        max: 0,
+        col: "#8fffff"
+    }, {
+        min: 0,
+        max: 5,
+        col: "#b0ffbc"
+    }, {
+        min: 5,
+        max: 10,
+        col: "#ffff73"
+    }, {
+        min: 10,
+        max: 15,
+        col: "#ffbe7d"
+    }, {
+        min: 15,
+        max: 20,
+        col: "#ff9b41"
+    }, {
+        min: 20,
+        max: 25,
+        col: "#ff5a41"
+    }, {
+        min: 25,
+        max: 30,
+        col: "#ff1e23"
+    }, {
+        min: 30,
+        max: 999,
+        col: "#fa3c96"
+    }],
+    
+    snowheight: [{
+        min: 0,
+        max: 10,
+        col: "#ffffb2"
+    }, {
+        min: 10,
+        max: 25,
+        col: "#b0ffbc"
+    }, {
+        min: 26,
+        max: 50,
+        col: "#8cffff"
+    }, {
+        min: 51,
+        max: 100,
+        col: "#19cdff"
+    }, {
+        min: 101,
+        max: 200,
+        col: "#1982ff"
+    }, {
+        min: 201,
+        max: 300,
+        col: "#0f5abe"
+    }, {
+        min: 301,
+        max: 400,
+        col: "#784bff"
+    }, {
+        min: 401,
+        max: 999,
+        col: "#cd0feb"
+    }
     ]
 };
