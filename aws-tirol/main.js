ce06c13501e3657cc0819ff36ded49afc2ac1dc3
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-04-22 17:19:22 +0200

    divIcon Label hinzugefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -44,12 +44,17 @@ L.control.scale({
 ).addTo(map);
 
 let newLabel = (coords, options) => {
-    console.log("Koordinaten coords: ", coords);
-    console.log("Optionsobjekt:", options);
-    let marker = L.marker([coords[1], coords[0]]);
-    console.log("Marker:", marker);
+    let. label = L.divIcon({
+        html: `<div>${options.value}</div>`,
+        className: "text-label"
+    })
+    let marker = L.marker([coords[1], coords[0]], {
+        icon:label
+    });
     return marker;
 };
+
+// .text-label
 
 
 let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
