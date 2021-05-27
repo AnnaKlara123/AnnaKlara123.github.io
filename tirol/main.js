da6de9dda98d2556aa5045306d072ced95e00370
Klara.Stang <anna.stang@student.uibk.ac.at>
2021-05-27 17:32:18 +0200

    for-Schreife zum setzen von HTML Element über ID

--- tirol/main.js
+++ tirol/main.js
@@ -176,7 +176,16 @@ const updateTexts = (nr) => {
         //console.log(etappe);
         // ist es die aktuelle Etappe?
         if (etappe.nr == nr) {
-            console.log("unsere Etappe", etappe);
+            //console.log("unsere Etappe", etappe);
+            for (let key in etappe) {
+                console.log("key:", key, "value:", etappe[key]);
+                //console.log("key:", key, "value:", etappe[key]);
+                // gibt es ein Element im HTML mit der ID von "key"
+                if (document.querySelector(`#text-${key}`)) {
+                    //console.log("Juhu", key, etappe[key]);
+                    document.querySelector(`#text-${key}`).innerHTML = etappe[key];
+                }
+            }
         }
     }
 };
@@ -213,4 +222,4 @@ pulldown.onchange = () => {
 map.on("zoomend moveend", () => {
     // Wikipedia Artikel zeichnen
     drawWikipedia(map.getBounds());
-});
+})
